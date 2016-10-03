e5d2c1efa3d49e28d98c0383998da8b23d63872e
Dexter Yan <yanshaocong@gmail.com>
2016-10-03 23:56:23 +0800

    add money cc

--- app.go
+++ app.go
@@ -1,28 +1,29 @@
 package main
 
 import (
-	"encoding/xml"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 )
 
 func main() {
-	type Stock struct {
-		Data string `xml:"data_sets>data_set>csv>data"`
-	}
-	url := "http://www.sl886.com/post/flashchart?c=01538"
+	// type Stock struct {
+	// 	Data string `xml:"data_sets>data_set>csv>data"`
+	// }
+	url := "http://money18.on.cc/js/daily/hk/quote/01538_d.js?t=1475503151919"
 	resp, err := http.Get(url)
 	if err != nil {
 
 	}
 
-	s := Stock{Data: ""}
+	// s := Stock{Data: ""}
+	// "http://money18.on.cc/chartdata/full/price/01538_price_full.txt"
+	// "http://money18.on.cc/info/liveinfo_quote.html?symbol=01538"
 	body, _ := ioutil.ReadAll(resp.Body)
-	err = xml.Unmarshal(body, &s)
-	if err != nil {
-
-	}
-	fmt.Print(string(s.Data))
+	// err = xml.Unmarshal(body, &s)
+	// if err != nil {
+	//
+	// }
+	fmt.Print(string(body))
 	fmt.Println("vim-go")
 }
